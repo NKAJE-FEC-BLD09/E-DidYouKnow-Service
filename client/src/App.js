d0c0f62c071abe5523f7e93d9c51cbacceb86d17
ep3824 <ethan.parent1@gmail.com>
2020-05-01 11:29:09 -0600

    Recreate module

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Did You Know?</h1>
+        <p>Content goes here</p>
+        <h4>Trivia</h4>
+        <p>Content goes here</p>
+        <h4>Goofs</h4>
+        <p>Content goes here</p>
+        <h4>Quotes</h4>
+        <p>Content goes here</p>
+        <h4>Alternate Versions</h4>
+        <p>Content goes here</p>
+        <h4>Connections</h4>
+        <p>Content goes here</p>
+        <h4>Soundtracks</h4>
+        <p>Content goes here</p>
+
+
+    </div>
+  );
+}
+
+export default App;
